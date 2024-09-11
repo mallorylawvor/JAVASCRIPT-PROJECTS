adf271b2dd0da9a15ec8e6ca88ea43ffc43e3151
mallorylawvor <75233074+mallorylawvor@users.noreply.github.com>
2024-09-11 06:26:27 -0700

    Create main.js

---
+++ Basic JavaScript Projects /Project1_expression_alert/JS/main.js
@@ -0,0 +1,29 @@
+window.alert("Hello, World!");
+document.write("docwrite");
+
+var A = "this is a string";
+document.write(A);
+
+var B = "She told him,\"leave me alone, or I\'ll tell Dad!"\;
+document.write(B);
+
+var C = "\"Be who you are and say what you feel," + "bcause those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seusss"
+document.write(C);
+
+var Family = "The Jones", Dad = "Tom", Mom = "Molly", Son = "Timmy;
+document.write(son);
+
+document.write(3 + 3);
+//excerices for JS course. 
+
+
+//assignment
+//window alert
+window.alert("JS Assignment");
+
+// two variables
+var sent1 = "this is the begiing of the string";
+var sent2 = "and this is the end of the string";
+
+//concatenate
+document.write(sent1 + sent2);
