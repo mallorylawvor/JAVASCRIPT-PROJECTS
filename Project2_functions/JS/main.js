0b4d4f6b9d309b787421dc14a7b414c7d66a970d
mallorylawvor <75233074+mallorylawvor@users.noreply.github.com>
2024-09-24 05:21:57 -0700

    Create main.js

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,18 @@
+var a = "Today is going to be,";
+var b = "cold with rain showers.";
+var c = (a + b);
+ function add(num1, num2) {
+    return num1 + num2;
+ }
+ var sum = add(5,7);
+
+function my_button_function() {
+   var str = "oh look a strawberry!";
+   document.getElementById("button_text").innerHTML = str;
+}
+
+function myFunction() {
+   var sentence = "I am learning";
+   sentence += " a lot from this book!";
+   document.getElementById("Concatenate").innerHTML = sentence;
+}
