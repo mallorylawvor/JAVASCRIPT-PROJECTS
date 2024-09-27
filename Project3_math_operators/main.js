6b89d943e24dbacd1f902085dd3ba25758ccdda9
mallorylawvor <75233074+mallorylawvor@users.noreply.github.com>
2024-09-26 19:22:22 -0700

    Create main.js

---
+++ Project3_math_operators/main.js
@@ -0,0 +1,44 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+
+}
+
+function substraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("math").innerHTML = "5 - 2 = " + Subtraction;
+}
+function multiplicaton() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 X 8 = " + simple_Math;
+}
+
+function divison () {
+    var simple_Math2 = 48 / 6;
+    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math2
+}
+
+function more_Math () {
+    var simple_Math3 = (1+2) * 10 / 2 -5;
+    document.getElementById(Math).innerHTML = "1 plus 2, multiplied by 10, divided in half and then subracted by 5 equals " + simple_Math3;
+}
+
+function modulus_Operator() {
+    var simple_Math4 = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math4; 
+}
+
+function negative_Operator() {
+    var X = 10;
+    document.getElementById("Math").innerHTML = -X;
+}
+ var Y = 5;
+ Y++;
+ document.write(Y):
+
+ var Z = 5.25;
+ Z--;
+ document.write(Z);
+
+window.alert(Math.random());
+window.alert(Math.random() * 100);
